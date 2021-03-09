ec4febca710a1630270af06bc5ed6e7433961ffe
Mikulas <mackazelena@gmail.com>
2021-03-09 13:50:48 +0100

    First Commit

---
+++ src/test/java/com/group9/NinjaGame/NinjaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group9.NinjaGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
