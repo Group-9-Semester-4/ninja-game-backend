ef64d8d3422f65950782382dff63f1dc90f6caf5
Mikulas <mackazelena@gmail.com>
2021-03-30 00:06:32 +0200

    Started styling admin panel

--- src/main/java/com/group9/NinjaGame/config/WebMVCConfig.java
+++ src/main/java/com/group9/NinjaGame/config/WebMVCConfig.java
@@ -1,29 +1,15 @@
 package com.group9.NinjaGame.config;
-import java.util.Locale;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 // import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
-import org.thymeleaf.spring5.ISpringTemplateEngine;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 
 @Configuration
@@ -74,7 +60,7 @@ public class WebMVCConfig implements WebMvcConfigurer,  ApplicationContextAware 
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**", "/css/**")
+        registry.addResourceHandler("/resources/**", "/static/css/**")
                 .addResourceLocations("/WEB-INF/resources/", "/WEB-INF/css/");
     }
 }
