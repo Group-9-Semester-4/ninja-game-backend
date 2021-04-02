33a5590192cc2de7572e4158bee3b26f96f7258e
Erik <erik.petra@afinode.com>
2021-04-02 11:24:13 +0200

    Changed engine to innodb

--- src/main/resources/db/migration/V1.4__CreateCardSetsTables.sql
+++ src/main/resources/db/migration/V1.4__CreateCardSetsTables.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS `card_sets` (
     `complete_time_limit` INT NOT NULL,
     `difficulty` INT NOT NULL,
     `multiplayer_suitable` BOOLEAN NOT NULL DEFAULT 1,
-    `temporary` BOOLEAN not null default 0);
+    `temporary` BOOLEAN not null default 0)
+    ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE IF NOT EXISTS `card_set_cards` (
     `card_id` BINARY(16) NOT NULL,
