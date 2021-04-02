1ec7932a5e676b4821b2fed4aa55fbd5f30b8066
Erik <erik.petra@afinode.com>
2021-04-02 11:38:36 +0200

    Removed primary key form card_set_cards table

--- src/main/resources/db/migration/V1.4__CreateCardSetsTables.sql
+++ src/main/resources/db/migration/V1.4__CreateCardSetsTables.sql
@@ -11,7 +11,6 @@ CREATE TABLE IF NOT EXISTS `card_set_cards` (
     `card_id` BINARY(16) NOT NULL,
     `card_set_id` BINARY(16) NOT NULL,
     FOREIGN KEY (card_id) REFERENCES cards (id) ON DELETE CASCADE,
-    FOREIGN KEY (card_set_id) REFERENCES card_sets (id) ON DELETE CASCADE,
-    PRIMARY KEY (card_id, card_set_id)
+    FOREIGN KEY (card_set_id) REFERENCES card_sets (id) ON DELETE CASCADE
     )
     ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
