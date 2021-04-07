d6be8f9db8c8f380639c3fb7a447757341f36b5e
Mikulas <mackazelena@gmail.com>
2021-04-07 19:10:58 +0200

    Worked on tests some more

--- src/test/java/com/group9/NinjaGame/api/GameResourceTest.java
+++ src/test/java/com/group9/NinjaGame/api/GameResourceTest.java
@@ -3,15 +3,15 @@ package com.group9.NinjaGame.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.group9.NinjaGame.entities.Game;
 import com.group9.NinjaGame.models.params.InitGameParam;
-import com.group9.NinjaGame.models.params.StartGameParam;
+import com.group9.NinjaGame.repositories.CardRepository;
+import com.group9.NinjaGame.repositories.CardSetRepository;
+import com.group9.NinjaGame.repositories.GameRepository;
 import com.group9.NinjaGame.resources.api.GameResource;
-import com.group9.NinjaGame.services.ICardService;
-import com.group9.NinjaGame.services.ICardSetService;
-import com.group9.NinjaGame.services.IGameService;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
+import com.group9.NinjaGame.services.GameService;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,14 +19,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultMatcher;
-
-import java.util.UUID;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -39,26 +37,35 @@ public class GameResourceTest {
     private ObjectMapper objectMapper;
 
     @MockBean
-    private ICardService cardService;
+    GameService gameService;
 
     @MockBean
-    private IGameService gameService;
+    GameRepository gameRepository;
 
     @MockBean
-    private ICardSetService cardSetService;
+    CardRepository cardRepository;
+
+    @MockBean
+    CardSetRepository cardSetRepository;
 
 
+    // TODO: check with ERIK, not working; empty response
     @Test
+    @Order(1)
     void initGameTest() throws Exception {
         InitGameParam testParam = new InitGameParam();
-        testParam.lobbyCode = "FLOP";
+        testParam.lobbyCode = "123456";
         testParam.multiPlayer = true;
-        testParam.playingAlone = false;
-        testParam.timeLimit =  420;
+        testParam.playingAlone = true;
+        testParam.timeLimit = 60;
+
         mockMvc.perform(post("/api/game/init")
-        .contentType(MediaType.valueOf("application/json"))
-        .content(objectMapper.writeValueAsString(testParam)))
-        .andExpect(status().isOk());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testParam)))
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.gameInfo").isNotEmpty());
+
+
     }
 
 //    @Test
@@ -79,27 +86,28 @@ public class GameResourceTest {
         mockMvc.perform(get("/api/game/cards"))
                 .andExpect(status().isOk());
     }
+
     @Test
     void getCardSetsTest() throws Exception {
         mockMvc.perform(get("/api/game/cardsets"))
                 .andExpect(status().isOk());
     }
+
     @Test
     void getGamesTest() throws Exception {
         mockMvc.perform(get("/api/game/games"))
-                .andExpect(status().isOk());
+                .andDo(MockMvcResultHandlers.print()).andExpect(status().isOk());
     }
 
     //TODO: almost working
     @Test
+    @Order(2)
     void getGameObjectsTest() throws Exception {
         MvcResult mvcResult = mockMvc.perform(get("/api/game/games")).andReturn();
-
         String json = mvcResult.getResponse().getContentAsString();
+        System.out.print(json);
         //Object actualObject = objectMapper.readValue(json, Game.class);
-        Game game = new Game();
-
-        assertThat(json).isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(game));
+        assertThat(json).isEqualToIgnoringWhitespace("[]");
     }
 
 }
