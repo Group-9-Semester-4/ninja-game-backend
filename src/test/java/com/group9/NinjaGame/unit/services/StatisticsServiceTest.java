400a672af36c31c7f5cb2cbf24b060734eb61a1d
Sam <sam.horacek@gmail.com>
2021-05-12 18:11:30 +0200

    added the InsertGameStatistics test
    - all the tests for StatisticsService done

--- src/test/java/com/group9/NinjaGame/unit/services/StatisticsServiceTest.java
+++ src/test/java/com/group9/NinjaGame/unit/services/StatisticsServiceTest.java
@@ -1,8 +1,14 @@
 package com.group9.NinjaGame.unit.services;
 
 import com.group9.NinjaGame.entities.Card;
+import com.group9.NinjaGame.entities.CardSet;
+import com.group9.NinjaGame.entities.Game;
+import com.group9.NinjaGame.entities.User;
 import com.group9.NinjaGame.entities.statisics.CardDiscard;
 import com.group9.NinjaGame.entities.statisics.CardRedraw;
+import com.group9.NinjaGame.entities.statisics.CardSetCompletion;
+import com.group9.NinjaGame.entities.statisics.TimePlayedPerGame;
+import com.group9.NinjaGame.models.params.FinishGameParam;
 import com.group9.NinjaGame.repositories.*;
 import com.group9.NinjaGame.services.CardService;
 import com.group9.NinjaGame.services.StatisticsService;
@@ -53,7 +59,67 @@ public class StatisticsServiceTest {
         statisticsService = new StatisticsService(cardSetRepository, gameRepository,cardDiscardRepository,
                 cardRedrawRepository, cardSetCompletionRepository, timePlayedPerGameRepository);
     }
+    @Test
+    public void testInsertGameStatistics() {
+        Card card = new Card();
+        card.setId(UUID.randomUUID());
+        card.setName("cardName101");
 
+        Card card1 = new Card();
+        card1.setId(UUID.randomUUID());
+        card1.setName("cardName202");
+
+        List<Card> listOfCards = new ArrayList<>();
+        listOfCards.add(card);
+        listOfCards.add(card1);
+
+        List<UUID> listOfRedrawnCards = new ArrayList<>();
+        listOfRedrawnCards.add(card1.getId());
+
+        CardSet cardSet = new CardSet(UUID.randomUUID(), "Random card set");
+        cardSet.setCards(listOfCards);
+        cardSet.setId(cardSetId);
+        Optional<CardSet> optionalCardSet = Optional.of(cardSet);
+
+        User user = new User();
+        user.setId(playerId);
+
+        Game game = new Game();
+        game.setId(UUID.randomUUID());
+        game.setSelectedCardSet(cardSet);
+        game.setUser(user);
+        Optional<Game> optionalGame = Optional.of(game);
+
+        FinishGameParam finishGameParam = new FinishGameParam();
+        finishGameParam.gameId = game.getId();
+        finishGameParam.cardSetId = cardSet.getId();
+        finishGameParam.cardsCompleted = cardSet.getCards().size();
+        finishGameParam.listOfRedrawnCards = listOfRedrawnCards;
+        finishGameParam.timeInSeconds = 25;
+        finishGameParam.unwantedCards = new ArrayList<>();
+
+
+        TimePlayedPerGame timePlayedPerGame = new TimePlayedPerGame(game.getId(), cardSet.getId(), finishGameParam.timeInSeconds, Instant.now());
+        CardSetCompletion cardSetCompletion = new CardSetCompletion(game.getId(), cardSet.getId(), (int) (((double) finishGameParam.cardsCompleted / (double) cardSet.getCards().size()) * 100), Instant.now());
+
+        doReturn(optionalCardSet).when(cardSetRepository).findById(finishGameParam.cardSetId);
+        doReturn(optionalGame).when(gameRepository).findById(finishGameParam.gameId);
+
+        doReturn(timePlayedPerGame).when(timePlayedPerGameRepository).save(any());
+        doReturn(cardSetCompletion).when(cardSetCompletionRepository).save(any());
+
+        doReturn(game).when(gameRepository).save(game);
+
+        Game gameResult = statisticsService.insertGameStatistics(finishGameParam);
+
+        assertNotNull(gameResult);
+        assertNotNull(timePlayedPerGame);
+        assertNotNull(cardSetCompletion);
+        assertEquals(gameResult.getTimeInSeconds(), timePlayedPerGame.getTimeInSeconds());
+        assertEquals(gameResult.getPercentageOfDoneCards(), cardSetCompletion.getPercentage());
+        assertEquals(gameResult.getTimeInSeconds(), game.getTimeInSeconds());
+        assertEquals(gameResult.getSelectedCardSet().getId(), cardSet.getId());
+    }
     @Test
     public void testInsertCardDiscards() {
         List<UUID> discardedCards = new ArrayList<>();
