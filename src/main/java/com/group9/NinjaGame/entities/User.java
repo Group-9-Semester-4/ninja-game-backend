4e21146fd742633edeec374b5e527d3bdc7da92b
Mikulas <mackazelena@gmail.com>
2021-05-26 17:14:47 +0200

    Created the backend stuff for users email tracking
    GameServices updates Last_visited timestamp, and creates registered_date when user is first registered

--- src/main/java/com/group9/NinjaGame/entities/User.java
+++ src/main/java/com/group9/NinjaGame/entities/User.java
@@ -3,6 +3,7 @@ package com.group9.NinjaGame.entities;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,7 +23,16 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Game> games;
 
-    public User() { }
+    @Column(name = "registered", nullable = false)
+    @NotBlank(message = "System error - registered date doesn't exist")
+    private Instant registered;
+
+    @Column(name = "last_visited", nullable = false)
+    @NotBlank(message = "System error - last_visited date doesn't exist")
+    private Instant last_visited;
+
+    public User() {
+    }
 
     public User(@NotBlank(message = "Email is mandatory") String email) {
         this.email = email;
@@ -42,5 +52,21 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Instant getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Instant registered) {
+        this.registered = registered;
+    }
+
+    public Instant getLast_visited() {
+        return last_visited;
+    }
+
+    public void setLast_visited(Instant last_visited) {
+        this.last_visited = last_visited;
     }
 }
