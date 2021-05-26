27de5a8730ea97a14f87bed4d703719a430267b4
Mikulas <mackazelena@gmail.com>
2021-05-26 16:27:23 +0200

    Changed users db
    Added registered and last_visited fields

--- src/main/resources/db/migration/V1.7__CreateUsersTable.sql
+++ src/main/resources/db/migration/V1.7__CreateUsersTable.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS `users` (
     `id` BINARY(16) NOT NULL PRIMARY KEY,
-    `email` VARCHAR(50) NOT NULL UNIQUE
+    `email` VARCHAR(50) NOT NULL UNIQUE,
+    `registered` timestamp not null,
+    `last_visited` timestamp not null
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
